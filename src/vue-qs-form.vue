fb42cae8bc972cefe0d5ab5342c4e29496acb4ea
xank <xank@qq.com>
2017-11-27 14:53:49 +0800

    init

---
+++ src/vue-qs-form.vue
@@ -0,0 +1,28 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'vue-qs-form',
+  props: {
+    data: {
+      required: true,
+      default: '',
+      type: Object,
+    }
+  },
+  data : () => ({
+  }),
+
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+  },
+
+  mounted () {
+  }
+}
+</script>
